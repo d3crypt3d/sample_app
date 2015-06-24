2ac90dfa52d0cf2cc1aef90f502c180ae6523ee1
John Doe <watcherspecial@gmail.com>
2015-06-24 21:30:17 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contacts"
+
+  root :to => 'static_pages#home'
+
+  match '/signup',   to: 'users#new'
+
+  match '/help',     to: 'static_pages#help'  
+  match '/about',    to: 'static_pages#about'  
+  match '/contacts', to: 'static_pages#contacts'  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
